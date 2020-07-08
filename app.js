55f7fe7a6ca88a9f13b470f8609aabe20fc5ab3c
TJReynolds88 <tj.reynolds1988@gmail.com>
2020-07-07 23:30:41 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ class UI {
                   class="product-img"
                 />
                 <button class="bag-btn" data-id=${product.id}>
-                  <i class="fas fa-shopping-cart"></i>add to bag
+                  <i class="fas fa-shopping-cart"></i>add to cart
                 </button>
               </div>
               <h3>${product.title}</h3>
@@ -134,11 +134,42 @@ class UI {
     closeCartBtn.addEventListener("click", this.hideCart);
   }
   populateCart(cart) {
-    cart.forEach(item => this.addCartItem(item));
+    cart.forEach((item) => this.addCartItem(item));
   }
   hideCart() {
     cartOverlay.classList.remove("transparentBcg");
     cartDOM.classList.remove("showCart");
+  }
+  cartLogic() {
+    // clear cart button
+    clearCartBtn.addEventListener("click", () => {
+      this.clearCart();
+    });
+    // cart functionality
+    cartContent.addEventListener("click", (event) => {
+      if (event.target.classList.contains("remove-item")) {
+        let removeItem = event.target;
+      }
+    });
+  }
+  clearCart() {
+    let cartItems = cart.map((item) => item.id);
+    cartItems.forEach((id) => this.removeItem(id));
+    while (cartContent.children.length > 0) {
+      cartContent.removeChild(cartContent.children[0]);
+    }
+    this.hideCart();
+  }
+  removeItem(id) {
+    cart = cart.filter((item) => item.id !== id);
+    this.setCartValues(cart);
+    Storage.saveCart(cart);
+    let button = this.getSingleButton(id);
+    button.disabled = false;
+    button.innerHTML = '<i class="fas fa-shopping-cart"></i>add to cart';
+  }
+  getSingleButton(id) {
+    return buttonsDOM.find((button) => button.dataset.id == id);
   }
 }
 
@@ -177,5 +208,6 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then(() => {
       ui.getBagButtons();
+      ui.cartLogic();
     });
 });
